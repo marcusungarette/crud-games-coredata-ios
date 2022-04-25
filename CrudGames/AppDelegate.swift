021270bdbd9c0aa12f8b65c5775bd0ef3554f849
marcusungarette <marcusungarette@gmail.com>
2022-04-25 00:11:22 -0300

    Setup all classes to respective TableView and ViewControllers, also added a config to AppDelegate handle UINavigation appearence

--- CrudGames/AppDelegate.swift
+++ CrudGames/AppDelegate.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+        UINavigationBar.appearance().tintColor = .orange
+        
         return true
     }
 
