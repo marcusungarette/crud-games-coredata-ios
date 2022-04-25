9e8ab0aab816ed774992bf85713f8e8ed7b80926
marcusungarette <marcusungarette@gmail.com>
2022-04-24 21:05:05 -0300

    Initial Commit

---
+++ CrudGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrudGames
+//
+//  Created by Marcus on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
